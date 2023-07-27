ee9f974e24bb78c9389ea44a96ccdeedd963be31
Karishma Chawla <kachawla@microsoft.com>
2023-07-26 17:26:38 -0700

    Add output result object

--- output.tf
+++ output.tf
@@ -25,4 +25,15 @@ output "secrets" {
   sensitive = true
 }
 
-
+output "result" {
+  value = {
+    values = {
+      host = "${kubernetes_service.redis.metadata[0].name}.${kubernetes_service.redis.metadata[0].namespace}.svc.cluster.local"
+      port = 6379
+    }
+    secrets = {
+      connectionString = "redis://${kubernetes_service.redis.metadata[0].name}.${kubernetes_service.redis.metadata[0].namespace}.svc.cluster.local:6379"
+    }
+  }
+  sensitive = true
+}
