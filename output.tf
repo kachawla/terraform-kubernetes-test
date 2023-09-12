6c6ea1c9040655c59f002807f272e54df6bf59dc
Karishma Chawla <kachawla@microsoft.com>
2023-09-12 12:19:40 -0700

    Update output.tf

--- output.tf
+++ output.tf
@@ -1,30 +1,3 @@
-output "host" {
-  value = "${kubernetes_service.redis.metadata[0].name}.${kubernetes_service.redis.metadata[0].namespace}.svc.cluster.local"
-}
-
-output "port" {
-  value = 6379
-}
-
-output "connectionString" {
-  value = "redis://${kubernetes_service.redis.metadata[0].name}.${kubernetes_service.redis.metadata[0].namespace}.svc.cluster.local:6379"
-  sensitive = true
-}
-
-output "values" {
-  value = {
-    host = "${kubernetes_service.redis.metadata[0].name}.${kubernetes_service.redis.metadata[0].namespace}.svc.cluster.local",
-    port = 6379,
-  }
-}
-
-output "secrets" {
-  value = {
-    connectionString = "redis://${kubernetes_service.redis.metadata[0].name}.${kubernetes_service.redis.metadata[0].namespace}.svc.cluster.local:6379"
-  }
-  sensitive = true
-}
-
 output "result" {
   value = {
     values = {
