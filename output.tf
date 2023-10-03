1b9dfb4657220d4b3921402ee879daa9f0e811bf
Karishma Chawla <kachawla@microsoft.com>
2023-10-03 08:52:02 -0700

    Update output.tf

--- output.tf
+++ output.tf
@@ -4,9 +4,5 @@ output "result" {
       host = "${kubernetes_service.redis.metadata[0].name}.${kubernetes_service.redis.metadata[0].namespace}.svc.cluster.local"
       port = 6379
     }
-    secrets = {
-      connectionString = "${kubernetes_service.redis.metadata[0].name}.${kubernetes_service.redis.metadata[0].namespace}.svc:6379"
-    }
   }
-  sensitive = true
 }
